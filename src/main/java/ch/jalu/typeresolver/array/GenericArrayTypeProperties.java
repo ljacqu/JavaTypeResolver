1d13506f9186c3797bf3884f228dce2540f34065
ljacqu <ljacqu@users.noreply.github.com>
2019-12-05 21:56:20 +0100

    Array properties: add javadoc + equals/hashCode/toString

--- src/main/java/ch/jalu/typeresolver/array/GenericArrayTypeProperties.java
+++ src/main/java/ch/jalu/typeresolver/array/GenericArrayTypeProperties.java
@@ -2,6 +2,7 @@ package ch.jalu.typeresolver.array;
 
 import java.lang.reflect.GenericArrayType;
 import java.lang.reflect.Type;
+import java.util.Objects;
 
 /**
  * Describes a generic array type: specifies the array dimension and the component type.
@@ -31,6 +32,12 @@ public class GenericArrayTypeProperties implements AbstractArrayProperties {
         this.dimension = dimension;
     }
 
+    /**
+     * Constructor for manual creation.
+     *
+     * @param componentType the component type
+     * @param dimension the dimension
+     */
     public GenericArrayTypeProperties(Type componentType, int dimension) {
         this.componentType = componentType;
         this.dimension = dimension;
@@ -54,5 +61,26 @@ public class GenericArrayTypeProperties implements AbstractArrayProperties {
     @Override
     public int getDimension() {
         return dimension;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        } else if (obj instanceof GenericArrayTypeProperties) {
+            GenericArrayTypeProperties that = (GenericArrayTypeProperties) obj;
+            return this.dimension == that.dimension && Objects.equals(this.componentType, that.componentType);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(dimension, componentType);
+    }
+
+    @Override
+    public String toString() {
+        return "GenericArrayTypeProperties[dimension=" + dimension + ",componentType='" + componentType + "']";
     }
 }
--- src/test/java/ch/jalu/typeresolver/array/ArrayClassPropertiesTest.java
+++ src/test/java/ch/jalu/typeresolver/array/ArrayClassPropertiesTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.not;
 
 /**
  * Test for {@link ArrayClassProperties}.
@@ -16,6 +17,32 @@ class ArrayClassPropertiesTest {
         testArrayDescription(String[].class, String.class, 1);
         testArrayDescription(short[][][][].class, short.class, 4);
         testArrayDescription(Integer.class, Integer.class, 0);
+    }
+
+    @Test
+    void shouldDefineProperEqualsAndHashCode() {
+        // given
+        ArrayClassProperties prop1 = new ArrayClassProperties(String.class, 2);
+        ArrayClassProperties prop2 = new ArrayClassProperties(String[][].class);
+        ArrayClassProperties prop3 = new ArrayClassProperties(double.class, 2);
+
+        // when / then
+        assertThat(prop1, equalTo(prop2));
+        assertThat(prop1.hashCode(), equalTo(prop2.hashCode()));
+        assertThat(prop1, not(equalTo(prop3)));
+        assertThat(prop1.hashCode(), not(equalTo(prop3.hashCode())));
+
+        assertThat(prop1.equals(prop1), equalTo(true));
+        assertThat(prop1.equals(new Object()), equalTo(false));
+    }
+
+    @Test
+    void shouldDefineToString() {
+        // given
+        ArrayClassProperties prop = new ArrayClassProperties(Integer.class, 2);
+
+        // when / then
+        assertThat(prop.toString(), equalTo(prop.getClass().getSimpleName() + "[dimension=2,componentType='class java.lang.Integer']"));
     }
 
     private static void testArrayDescription(Class<?> givenArrayClass,
